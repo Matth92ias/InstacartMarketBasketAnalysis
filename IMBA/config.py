c2c30fb8a103917eb115eac0dd3144d9fb4457c0
Matthias Lenfers <M.Lenfers@gmx.net>
2020-11-30 19:29:50 +0100

    einige aenderungen

--- IMBA/config.py
+++
@@ -1,4 +0,0 @@
-from pathlib import Path  # pathlib is seriously awesome!
-
-data_dir = Path('~/code/InstacartMarketBasketAnalysis/data')
-#data_path = data_dir / 'my_file.csv'  # use feather files if possible!!!
